30c010b01e24979fb7deca748c02addc79120953
Diego Riquelme <diego.riquelme.s@usach.cl>
2021-11-09 23:04:08 -0300

    Reimaginación de revokeAllAccess
    Se rehizo la función con el fin de obtener otra forma de borrar a los usuarios pertenecientes a un documento, si bien, se consiguió borrar un documento a la vez, aún no se logra borrar todos los documentos con una lista de Id's creada a partir del user.

--- testBorrar.rkt
+++ testBorrar.rkt
@@ -3,56 +3,55 @@
 
 (define listaTest '((1 "driques" (30 10 2020) "primer documento" "doc1") (3 "driques" (30 10 2020) "tercer documento" "doc3") (2 "pepe" (30 10 2020) "segundooo documento" "doc2")))
 
-(define listaAccess '((2 (("driques" #\w) ("pepe3" #\r)) 1 (("pepe3" #\w)))))
+(define listaAccess '((2 (("driques" #\w) ("pepe3" #\r)) 1 (("pepe5" #\w)) 4 (("driques" #\w) ("pepe3" #\r)) 3 (("pepe3" #\r)))))
 (define listaAccess2 '((1 (("pepe3" #\w)))))
+(define listaAccessTest '( (2 (("driques" #\w) ("pepe3" #\r))) (1 (("pepe3" #\w))) (4 (("driques" #\w) ("pepe3" #\r))) (3 (("pepe3" #\r)))) )
 
+;-----------------------------------------------------------------------------
 (define (nameEq? user)
             (lambda (listaDocs)
               (if (eq? (cadr listaDocs) user)
                   #t
-                  #f))
+                  #f)
+              )
   )
-
-
 
 
 (define (filtraId listaDocs user)
        (map car (filter (nameEq? user) listaDocs))
+       
   )
-
 ;------------------------------------------------------------------------------
-(define (idEq? id)
-          (lambda (listaAccess)
-            (if (eq? (car listaAccess) id)
-               null
-               (list ( (car listaAccess) (car (cdr listaAccess))))
-               
-               )
-            )
-  )
-(define (idEq?2 id)
-   (lambda (listaAccess)
-     (if (eq? id (car listaAccess))
-         '()
-         (list id (car listaAccess));(list (car listaAccess) (cadr listaAccess))
-       )
-     )
-  )
 
-(define (revoke listaAccess ids listaFinal)
-               (if (< 2 (length listaAccess))
-                  (revoke (cddr listaAccess) (cdr ids) (append ((idEq?2 (car ids)) listaAccess) listaFinal)) ;(revoke (cddr listaAccess) (cdr ids) (append (filter (idEq? (car ids)) listaAccess) listaFinal) )
-                  ((idEq?2 ids) listaAccess) ; listaFinal)
-               
+(define (usuariosRemover id listaAccess)
+         (if (null? listaAccess)
+             '()
+               (if (eq? (car listaAccess) id)
+                   (cadr listaAccess)
+                   (usuariosRemover id (cddr listaAccess))
+                   )
+             )
          )
+
+
+(define (filtraIdDado id listaAccess)
+           (remove (usuariosRemover id listaAccess) listaAccess)
+         
+          
   )
-  
+
+(define (eliminaId id ListaAccess)
+   (remq id (filtraIdDado id (car listaAccess)) )
+  ) 
+;------------------------------------------------------------------------------
+
+(define (revokeAllAccesses listaAccess listaId verif listaComp)
+   null
+       
+  )
+
+;(map (lambda (id listaAccess) (remq id (filtraIdDado id (car listaAccess)) )) (car listaAccess))
+
+;(revokeAllAccessess listaAccess '(1 3))
 
 
-;Lo que busco hacer es volver a crear listaAccess pero con un parentesis más, es decir '(((1 (("pepe3" #\w)))))) esto para encapsular la lista donde se encuentren los usuarios, para luego
-;Con filter descartarlo y hacer una lista nueva, despues de eso, recursivamente, volveré a preguntar sobre la lista que ya se filtró pero ahora con el siguiente id.
-
-
-;> (filtraNombre listaTest "driques")
-;'((1 "driques" (30 10 2020) "primer documento" "doc1") (3 "driques" (30 10 2020) "tercer documento" "doc3"))
-;(revoke (car listaAccess) (filtraId listaTest "driques") '())
